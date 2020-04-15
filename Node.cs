9141dd56b02da09e10ecdf6dfa1aec5ce1d274e0
charles-cohn <cohncl@gmatc.matc.edu>
2020-04-14 23:15:17 -0500

    Assignment #6 Submission

---
+++ Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BinarySearchTreeProject
+{
+    class Node
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+
+        public Node lchild;
+        public int info;
+        public Node rchild;
+
+        public Node(int i) 
+        {
+            info = i;
+            lchild = null;
+            rchild = null;
+        }
+    }
+}
